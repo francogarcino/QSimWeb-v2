d2da268cf61786c5966be9ccd2c28549877f0f09
Agustin Lascar <aguslascar@hotmail.com.ar>
2024-09-20 15:15:01 -0300

    change result font to match

--- src/arch-views/CodeExecutor.js
+++ src/arch-views/CodeExecutor.js
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   results: {
-    fontFamily: 'mono'
+    fontFamily: 'monospace'
   },
   fab: (isMobile ? {
     position: 'absolute',
